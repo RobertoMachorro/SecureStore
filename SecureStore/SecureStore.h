c13fd4bd8b569e654cc39ec75e139c1fa8e9bfb7
Roberto Machorro <RobertoMachorro@users.noreply.github.com>
2019-07-20 12:31:38 -0400

    Initial commit, Xcode Mac Framework template.

---
+++ SecureStore/SecureStore.h
@@ -0,0 +1,19 @@
+//
+//  SecureStore.h
+//  SecureStore
+//
+//  Created by Roberto Machorro on 7/20/19.
+//  Copyright © 2019 Unplugged Ideas, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SecureStore.
+FOUNDATION_EXPORT double SecureStoreVersionNumber;
+
+//! Project version string for SecureStore.
+FOUNDATION_EXPORT const unsigned char SecureStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecureStore/PublicHeader.h>
+
+
