0e1dcd350e8a0e7dbd59789afdc49a193e5eddb8
Roberto Machorro <RobertoMachorro@users.noreply.github.com>
2019-07-23 21:25:26 -0400

    Added mobile version of Framework.

---
+++ SecureStoreMobile/SecureStoreMobile.h
@@ -0,0 +1,19 @@
+//
+//  SecureStoreMobile.h
+//  SecureStoreMobile
+//
+//  Created by Roberto Machorro on 7/23/19.
+//  Copyright © 2019 Unplugged Ideas, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SecureStoreMobile.
+FOUNDATION_EXPORT double SecureStoreMobileVersionNumber;
+
+//! Project version string for SecureStoreMobile.
+FOUNDATION_EXPORT const unsigned char SecureStoreMobileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecureStoreMobile/PublicHeader.h>
+
+
